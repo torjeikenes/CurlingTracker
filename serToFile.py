acfdae09c980e4e3578e8b4d6684c3a3e951e771
torjeikenes <torje.n.eikenes@gmail.com>
2019-11-18 15:28:43 +0100

    overwriter filen

--- serToFile.py
+++ serToFile.py
@@ -1,23 +1,3 @@
-###############
-### Script listens to serial port and writes contents into a file
-###############
-### requires pySerial to be installed 
-#import serial  # sudo pip install pyserial should work
-#
-#serial_port = '/dev/ttyACM0';
-#baud_rate = 9600; #In arduino, Serial.begin(baud_rate)
-#write_to_file_path = "output.txt";
-#
-#output_file = open(write_to_file_path, "w+");
-#ser = serial.Serial(serial_port, baud_rate)
-#while True:
-#    line = ser.readline();
-#    line = line.decode("utf-8") #ser.readline returns a binary, convert to string
-#    print(line);
-#    output_file.write(line);
-
-
-
 import serial
 arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=.1)
 
@@ -28,6 +8,6 @@ while True:
     if len(data) > 0:
         print data
         line = data.decode("utf-8") #ser.readline returns a binary, convert to string
-        with open(write_to_file_path, 'a') as the_file:
+        with open(write_to_file_path, 'w') as the_file:
             the_file.write(line)
-            the_file.write("\n")
+            #the_file.write("\n")
